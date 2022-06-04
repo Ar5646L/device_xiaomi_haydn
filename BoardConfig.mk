6fe49fc6f11a1209033d907d7f3f5dccfe6c97ee
Chenyang Zhong <zhongcy95@gmail.com>
2022-06-04 08:05:54 -0400

    haydn: Build kernel with Prelude Clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,9 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_CONFIG := vendor/haydn-qgki_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/haydn
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 cgroup.memory=nokmem,nosocket console=ttyMSM0,115200n8 loop.max_part=7 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=0 pcie_ports=compat iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1
+TARGET_KERNEL_CLANG_VERSION := prelude
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-prelude/bin
+KERNEL_SUPPORTS_LLVM_TOOLS := true
 
 # Kernel modules
 BOOT_KERNEL_MODULES := \
