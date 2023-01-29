66485c7971a70b33dc5d57c9774e6f86f817c6ef
Ar5646L <aryan.v.bhatt@gmail.com>
2023-01-29 07:56:07 +0000

    haydn: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,17 @@
+echo 'Starting to clone stuffs needed for your device'
+
+echo 'Cloning Kernel tree [1/4]'
+# Kernel Tree
+git clone https://github.com/PixelExperience-Devices/kernel_xiaomi_haydn -b thirteen kernel/xiaomi/haydn
+
+echo 'Cloning Vendor tree [2/4]'
+# Vendor Tree
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_haydn -b thirteen vendor/xiaomi/haydn
+
+echo 'Cloning Firmware [3/4]'
+# Firmware
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
+
+echo 'Cloning Prelude Clang [4/4]'
+# Prelude Clang
+git clone --depth=1 https://gitlab.com/jjpprrrr/prelude-clang.git prebuilts/clang/host/linux-x86/clang-prelude
